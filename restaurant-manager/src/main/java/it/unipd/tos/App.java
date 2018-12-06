b58fba38965ce82183eb6458d200b433288d9ac9
PardeepOne <pardeeps460@gmail.com>
2018-12-06 23:20:17 +0100

    Trying to improve coverage

--- restaurant-manager/src/main/java/it/unipd/tos/App.java
+++ restaurant-manager/src/main/java/it/unipd/tos/App.java
@@ -3,14 +3,10 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.tos;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        
     }
 }
